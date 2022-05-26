07b736d4b968209ee100325fffeeddf52637e8ae
raj kumar <88712283+raja-rk@users.noreply.github.com>
2022-05-26 12:59:07 +0530

    initials

---
+++ demo.js
@@ -0,0 +1,22 @@
+/*javascript is a programming language 
+orignally designed to run on browsers
+but now 'node' can be used to run js code
+node- it is c++ program to execute js code outside the browser
+
+*/
+console.log("Hello World")
+//java variables
+var num1=29;
+var num2= 20.5
+var str="raj"
+var str1='raj';
+//objects
+var marks={
+    x:1,
+    y:2,
+    z:3
+}
+//booleans
+var a=true
+var b=false
+//undefned or null
